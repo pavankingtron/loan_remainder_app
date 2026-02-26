c2392e8eb69dbbd20880f7b56027fafe7c32d6f7
Pavan Kumar <pavankumarm9999@gmail.com>
2026-02-26 10:33:08 +0530

    Fix SQLite absolute path

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
 
@@ -7,7 +9,7 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///data.db"
+        "sqlite:///" + os.path.join(BASE_DIR, "data.db")
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
